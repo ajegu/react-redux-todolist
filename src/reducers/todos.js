1d2e82d75a5e13858219b00ea6f34786e1acd2c3
Allan JEGU <allanjegu@gmail.com>
2017-03-20 16:47:54 +0100

    using object spread operator

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -11,10 +11,10 @@ const todo = (state = {}, action) => {
                 return state
             }
 
-            return Object.assign({}, state, {
+            return {
+                ...state,
                 completed: !state.completed
-            })
-
+            }
         default:
             return state
     }
